3e13ed25d6adce3ce4aa641867e271591aa2bbf5
Tsubashi <8th.realm@gmail.com>
2014-11-26 12:42:01 -0700

    Added persistance to the checkmarks (if enabled)

--- jesidget/var/mobile/Library/iWidgets/Jesidget/DataStore.js
+++ jesidget/var/mobile/Library/iWidgets/Jesidget/DataStore.js
@@ -8,6 +8,10 @@ $( document ).ready(function() {
     if ("noteID" in window && noteID != "None") {
       $("#myText").val(localStorage.getItem(noteID));
       $("#name").html(noteID);
+      if (localStorage.getItem(noteID+"-NOT_A_TITLE-ITICOY") == "yes") { // Is The Item Checked Off Yet?
+        $('#check-not_done').fadeOut();
+        $('#check-done').fadeIn();
+      }
     } else {
       $("#myText").val("Remember, this note is not persistant (Ie. it will be erased after a respring/reboot). To make a persistant note, make sure to specify a note name when adding it to the lockscreen."); 
       $("#name").html("Temporary Note");
@@ -18,25 +22,27 @@ $( document ).ready(function() {
   
   //////////////////////////////
   // Change note color
-  switch(noteColor) {
-    case 1: //Blue
-      $('#myText').css({background:"linear-gradient(#afb9f9, #8d9bf7)"});
-    break;
-    case 2: //Green
-      $('#myText').css("background","linear-gradient(#def9af, #d0f78d)");
-    break;
-    case 3: //Pink
-      $('#myText').css("background","linear-gradient(#f9afb9, #f78d9b)");
-    break;
-    case 4: //Purple
-      $('#myText').css("background","linear-gradient(#caaff9, #b48df7)");
-    break;
-    case 5: //Teal
-      $('#myText').css("background","linear-gradient(#aff9ef, #8df7e9)");
-    break;
-    default:
-      $('#myText').css("background","linear-gradient(#F9EFAF, #F7E98D)");
-    break;
+  if ("noteColor" in window) {
+    switch(noteColor) {
+      case 1: //Blue
+        $('#myText').css({background:"linear-gradient(#afb9f9, #8d9bf7)"});
+      break;
+      case 2: //Green
+        $('#myText').css("background","linear-gradient(#def9af, #d0f78d)");
+      break;
+      case 3: //Pink
+        $('#myText').css("background","linear-gradient(#f9afb9, #f78d9b)");
+      break;
+      case 4: //Purple
+        $('#myText').css("background","linear-gradient(#caaff9, #b48df7)");
+      break;
+      case 5: //Teal
+        $('#myText').css("background","linear-gradient(#aff9ef, #8df7e9)");
+      break;
+      default:
+        $('#myText').css("background","linear-gradient(#F9EFAF, #F7E98D)");
+      break;
+    }
   }
   
   //////////////////////////////
@@ -45,11 +51,13 @@ $( document ).ready(function() {
     alert("Congratulations! You did it!");
     $('#check-not_done').fadeOut();
     $('#check-done').fadeIn();
+    localStorage.setItem(noteID+"-NOT_A_TITLE-ITICOY", "yes"); // Is The Item Checked Off Yet?
   });
   
   $('#check-done').click(function() {
     $('#check-done').fadeOut();
     $('#check-not_done').fadeIn();
+    localStorage.setItem(noteID+"-NOT_A_TITLE-ITICOY", "no"); // Is The Item Checked Off Yet?
   });
 
   //////////////////////////////
